b0cb2e59363d99a71b19502f228be410c665f768
KMURASEofficial <KMURASEofficial@gmail.com>
2023-07-12 13:56:13 +0900

    deleted

--- ipcc7/crypto_pke/multigraph/decrypt.h
+++
@@ -1,9 +0,0 @@
-#ifndef DECRYPTION_H
-#define DECRYPTION_H
-
-#include "params.h"
-#include "invpoly.h"
-
-void get_pdsarr(uint8_t pdsarr[VERTEXES], const uint8_t *pds);
-int decrypt(uint32_t *m, invpoly *ct, uint8_t pdsarr[VERTEXES]);
-#endif
